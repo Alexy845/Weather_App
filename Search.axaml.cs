84e6747c1570adb83ab6438afa35b98933933222
ahliko <killian.guillemot@ynov.com>
2023-10-30 13:25:03 +0100

    feat(api): Update WeatherApi.cs and Search.axaml.cs
    - Update BaseUrl in WeatherApi.cs to include city name and country code
    - Rename AllCity variable to weather in WeatherApi.cs
    - Modify GetWeatherForecast method in WeatherApi.cs to return ApiClass object
    - Add Coord, Weather, Main, Wind, Clouds, Rain, Snow, Sys classes in ApiClass in WeatherApi.cs
    - Update Search.axaml to include a CountryBox for entering country code
    - Update Search.axaml.cs to use the WeatherApi class for retrieving weather data
    - Modify GetWeather method in Search.axaml.cs to accept both city name and country code
    - Parse the retrieved weather data and display it in the ResultsBox ListBox
    
    Issue: API

--- Search.axaml.cs
+++ Search.axaml.cs
@@ -1,18 +1,12 @@
-using System;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using Avalonia;
+using System.Collections.ObjectModel;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
-using Avalonia.Markup.Xaml;
-using Microsoft.CodeAnalysis.CSharp;
 
 namespace Weather_App;
 
 public partial class Search : Window
 {
-    public WeatherApi weatherApi = new WeatherApi();
+    public WeatherApi WeatherApi = new WeatherApi();
     public Search()
     {
         InitializeComponent();
@@ -20,39 +14,70 @@ public partial class Search : Window
 
     private void SearchButton_Click(object? sender, RoutedEventArgs routedEventArgs)
     {
-        GetWeather(searchBox.Text);
+        if (SearchBox.Text != null && CountryBox.Text != null)
+                GetWeather(SearchBox.Text, CountryBox.Text);
     }
-    
-    public async void GetWeather(string ville)
-    {
-        weatherApi.GetCity(ville);
-        while (!weatherApi.Ok)
-        {
-                
-        }
-        weatherApi.Ok = false;
-        foreach (var i in weatherApi.AllCity)
-        {
-            
-                var weather = await weatherApi.GetWeatherForecast(i.lat, i.lon);
-                if (weather == null || !RemoveAccents(weather.name.ToLower()).Contains(RemoveAccents(ville.ToLower())))
-                    continue;
-                Console.WriteLine($"{weather.name} : {weather.id} -> {weather.coord.lon} ; {weather.coord.lat}" );
-        }
-    }
-    static string RemoveAccents(string input)
-    {
-        string normalized = input.Normalize(NormalizationForm.FormD);
-        StringBuilder builder = new StringBuilder();
 
-        foreach (char c in normalized)
+    public async void GetWeather(string ville, string pays)
+    {
+        var weather = await WeatherApi.GetWeatherForecast(ville, pays);
+        if (weather != null)
         {
-            if (CharUnicodeInfo.GetUnicodeCategory(c) != UnicodeCategory.NonSpacingMark)
+            var items = new ObservableCollection<string>(){};
+            var coord = weather.coord;
+            var weatherList = weather.weather;
+            var main = weather.main;
+            var wind = weather.wind;
+            var sys = weather.sys;
+            var clouds = weather.clouds;
+            var rain = weather.rain;
+            var snow = weather.snow;
+            var dt = weather.dt;
+            var id = weather.id;
+            var name = weather.name;
+            var cod = weather.cod;
+            var baseWeather = weather.@base;
+            var visibility = weather.visibility;
+            var timezone = weather.timezone;
+
+            items.Add($"Coord : {coord.lon} ; {coord.lat}");
+            items.Add($"Weather : {weatherList[0].id} ; {weatherList[0].main} ; {weatherList[0].description} ; {weatherList[0].icon}");
+            items.Add($"Main : {main.temp} ; {main.feels_like} ; {main.temp_min} ; {main.temp_max} ; {main.pressure} ; {main.humidity} ; {main.sea_level} ; {main.grnd_level}");
+            items.Add($"Wind : {wind.speed} ; {wind.deg} ; {wind.gust}");
+            items.Add($"Sys : {sys.type} ; {sys.id} ; {sys.country} ; {sys.sunrise} ; {sys.sunset}");
+            items.Add($"Clouds : {clouds.all}");
+            if (rain is not null)
             {
-                builder.Append(c);
+                if (rain._1h != 0)
+                {
+                    items.Add($"Rain : {rain._1h}");
+                }
+                else
+                {
+                    items.Add($"Rain : {rain._3h}");
+                }
             }
+            
+            if (snow is not null)
+            {
+                if (snow._1h != 0)
+                {
+                    items.Add($"Snow : {snow._1h}");
+                }
+                else
+                {
+                    items.Add($"Snow : {snow._3h}");
+                }
+            }
+            items.Add($"Dt : {dt}");
+            items.Add($"Id : {id}");
+            items.Add($"Name : {name}");
+            items.Add($"Cod : {cod}");
+            items.Add($"Base : {baseWeather}");
+            items.Add($"Visibility : {visibility}");
+            items.Add($"Timezone : {timezone}");
+            
+            ResultsBox.ItemsSource = items;
         }
-
-        return builder.ToString();
     }
 }
--- WeatherApi.cs
+++ WeatherApi.cs
@@ -11,59 +11,56 @@ public class WeatherApi
 {
     private readonly HttpClient _client;
     private const string ApiKey = "2a2922af3fb2b058c284c0fc51f3348e";
-    private const string BaseUrl = "https://api.openweathermap.org/data/2.5/weather?lat={city id}&appid=";
-    public List<City1>? AllCity;
+
+    private const string BaseUrl =
+        "https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid=";
 
     public WeatherApi()
     {
         _client = new HttpClient();
     }
 
-    public bool Ok { get; set; }
-
-    public async void GetCity(string ville)
+    public async Task<ApiClass> GetWeatherForecast(string name, string country)
     {
-        ville = ville.Replace(" ", "+");
-        string requestUrl = $"http://api.openweathermap.org/geo/1.0/direct?q={ville}&limit=5&appid={ApiKey}";
+        string requestUrl = $"https://api.openweathermap.org/data/2.5/weather?q={name},{country}&appid={ApiKey}";
         try
         {
             HttpResponseMessage response =  await _client.GetAsync(requestUrl);
             response.EnsureSuccessStatusCode();
-            AllCity = JsonConvert.DeserializeObject<List<City1>>(await response.Content.ReadAsStringAsync());
-            Ok = true;
+            var weather = JsonConvert.DeserializeObject<ApiClass>(await response.Content.ReadAsStringAsync());
+            Console.WriteLine($"{weather.name} : {weather.id} -> {weather.coord.lon} ; {weather.coord.lat}" );
+            return weather;
         }
         catch(HttpRequestException e)
         {
             Console.WriteLine($"Erreur : {e.Message}");
+            return null;
         }
     }
+}
 
-    public async Task<ApiClass?> GetWeatherForecast(float lat, float lon)
+public class ApiClass
+{
+    public Coord coord { get; set; }
+
+    public class Coord
     {
-        var response = await _client.GetAsync($"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={ApiKey}");
-        if (!response.IsSuccessStatusCode)
-            return null;
-        response.EnsureSuccessStatusCode();
-        return JsonConvert.DeserializeObject<ApiClass>(await response.Content.ReadAsStringAsync());
+        public float lon { get; set; }
+        public float lat { get; set; }
     }
-    
-    public class ApiClass
+
+    public List<Weather> weather { get; set; }
+
+    public class Weather
     {
-        public Coord coord { get; set; }
-        public Weather[] weather { get; set; }
-        public string Base { get; set; }
-        public Main main { get; set; }
-        public int visibility { get; set; }
-        public Wind wind { get; set; }
-        public Clouds clouds { get; set; }
-        public int dt { get; set; }
-        public Sys sys { get; set; }
-        public string name { get; set; }
         public int id { get; set; }
-        public int cod { get; set; }
-        public int timezone { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
     }
-    
+
+    public string @base { get; set; }
+    public Main main { get; set; }
 
     public class Main
     {
@@ -73,67 +70,57 @@ public class WeatherApi
         public float temp_max { get; set; }
         public int pressure { get; set; }
         public int humidity { get; set; }
+        public int sea_level { get; set; }
+        public int grnd_level { get; set; }
     }
+
+    public int visibility { get; set; }
+    public Wind wind { get; set; }
 
     public class Wind
     {
         public float speed { get; set; }
         public int deg { get; set; }
+        public float gust { get; set; }
     }
+
+    public Clouds clouds { get; set; }
 
     public class Clouds
     {
         public int all { get; set; }
     }
+    
+    public Rain rain { get; set; }
+
+    public class Rain
+    {
+        public float _1h { get; set; }
+        public float _3h { get; set; }
+    }
+    
+    public Snow snow { get; set; }
+    
+    public class Snow
+    {
+        public float _1h { get; set; }
+        public float _3h { get; set; }
+    }
+
+    public int dt { get; set; }
+    public Sys sys { get; set; }
 
     public class Sys
     {
         public int type { get; set; }
         public int id { get; set; }
-        public float message { get; set; }
         public string country { get; set; }
         public int sunrise { get; set; }
         public int sunset { get; set; }
     }
-}
 
-
-
-
-
-
-
-
-public class Coord
-{
-    public float lon { get; set; }
-    public float lat { get; set; }
-}
-
-public class Weather
-{
+    public int timezone { get; set; }
     public int id { get; set; }
-    public string main { get; set; }
-    public string description { get; set; }
-    public string icon { get; set; }
-}
-
-public class City
-{
-    public float id { get; set; }
     public string name { get; set; }
-    public Coord coord { get; set; }
-    public string country { get; set; }
-    public string state { get; set; }
-    
-}
-
-
-public class City1
-{
-    public string name { get; set; }
-    public float lat { get; set; }
-    public float lon { get; set; }
-    public string country { get; set; }
-    public string state { get; set; }
+    public int cod { get; set; }
 }
